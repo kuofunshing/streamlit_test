489d83ddcdf9090c9e67908e362a27dbfe6da260
kuofunshing <163220723+kuofunshing@users.noreply.github.com>
2024-06-24 14:08:59 +0800

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -2,6 +2,7 @@ import streamlit as st
 import os
 import openai
 from openai import OpenAI
+from PIL import Image
 
 # 使用环境变量设置 OpenAI API 金钥
 api_key = os.getenv("OPENAI_API_KEY")
@@ -28,10 +29,8 @@ if user_input:
         chat_completion = client.chat.completions.create(
             messages=st.session_state['chat_history'],
             model="gpt-4o",  # 假设这是正确的模型名称
-            max_tokens=200,  # 限制回应的最大字元数
-            stop=["\n"]  # 可以设定停止符号，例如遇到换行停止生成
         )
-        assistant_message = chat_completion.choices[0].message.content.strip()
+        assistant_message = chat_completion.choices[0].message.content
         st.session_state['chat_history'].append({"role": "assistant", "content": assistant_message})
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
@@ -40,3 +39,30 @@ if user_input:
 for message in st.session_state['chat_history']:
     role = "你" if message["role"] == "user" else "ChatGPT"
     st.write(f"{role}: {message['content']}")
+
+st.header("圖片")
+st.write("这是图片页面。")
+animal = st.selectbox("选择一个动物", ['cat', 'Pig','Bus','Cheetah','Penguins','Car','rabbit','zebra','Scooter'])
+
+# 根据选择显示图片和文本
+if animal:
+    image_path = f'/label/{animal}.jpg'
+    text_path = f'/label/{animal}.txt'
+    
+    if os.path.exists(image_path) and os.path.exists(text_path):
+        image = Image.open(image_path)
+        st.image(image, caption=f'显示的是: {animal}', use_column_width=True)
+        
+        with open(text_path, 'r') as file:
+            text_content = file.read()
+        st.write(text_content)
+    else:
+        st.error("文件不存在，请确保路径和文件名正确。")
+
+uploaded_file = st.file_uploader("选择一个图片文件", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='上传的图片', use_column_width=True)
+else:
+    st.write("请上传一个图片文件。")
